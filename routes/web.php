f12243b0ee3ac4a85230632b08d185fddbb51fd1
Marcelino Mejia <mmejia1@tecbaco.com>
2021-02-04 10:45:46 -0500

    Seledio esilo a las vistas restablecer password y restabelcer cuenta

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Route de vancate
+Route::get('/vacantes', 'VacanteController@index')->name('vancate.index');
+Route::get('/vacantes/create', 'VacanteController@create')->name('vancate.create');
